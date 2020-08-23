0427fc13ef7facf1560573c26af1f6d0765cee34
jackbuzzhealy001 <jackbuzzhealy001@outlook.com>
2020-08-23 23:30:21 +0000

    app2 partially completed

--- application2/applications/routes.py
+++ application2/applications/routes.py
@@ -2,27 +2,34 @@ from applications import app
 from flask import Response, request
 from random import randint 
 
-@app.route('/getAnimal', methods=['GET'])
-def getLighteningBall():
-    
-    return Response(animal, mimetype="text/plain")
+@app.route('/getMyLighteningBall', methods=['GET'])
+def getMyLighteningBall():
+    myLighteningBall = random.randint(1,14)
+    return Response(myLighteningBall, mimetype="number/plain")
 
-@app.route('/getNoise', methods=['POST'])
-def getNoise():
-    animal = request.data.decode('utf-8')
-    if animal == "Dog":
-        noise = "Woof!"
-    elif animal == "Cat":
-        noise = "Meow!"
-    elif animal == "Mouse":
-        noise = "Squeak!"
-    elif animal == "Horse":
-        noise = "Neigh!"
-    elif animal == "Cow":
-        noise = "Moo!"
-    elif animal == "Sheep":
-        noise = "Baa!"
-    else:
-        noise = "Unknown!"
-    
-    return Response(noise, mimetype='text/plain')
+@app.route('/postMyLighteningBall', methods=['POST'])
+def postMyLighteningBall():
+    myLighteningBall = request.data.decode('utf-8')
+    winningLighteningBall = random.randint(1,14)
+    return Response(winningLighteningBall, mimetype='number/plain')
+
+@app.route('/getMyLotteryNumbers', methods=['GET'])
+def getMyLotteryNumbers():
+    count = 1
+    myLotteryNumbers = []
+    while count != 6:
+        myLotteryNumbers.append(random.randint(1,50)
+        count = count + 1
+
+    return Response(LotteryNumbers, mimetype="number/plain")
+
+@app.route('/postMyLotteryNumbers', methods=['POST'])
+def postMyLotteryNumbers():
+    myLotteryNumbers = request.data.decode('utf-8')
+    count = 1
+    winningLotteryNumbers = []
+    while count != 6:
+        winningLotteryNumbers.append(random.randint(1,50)
+        count = count + 1
+
+    return Response(winningLotteryNumbers, mimetype='number/plain')

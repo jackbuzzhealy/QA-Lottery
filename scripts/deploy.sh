d69dc690817c86a0e91412d69603567bce58f04c
jackbuzzhealy001 <jackbuzzhealy001@outlook.com>
2020-09-01 05:59:13 +0000

    jenkins

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 cd QA-Lottery
 
+export=DATABASE_URI=mysql+pymysql://root:megabrick55@34.105.176.207:3306/qa_lottery_db
 sudo chown jenkins /var/run/docker.sock
 sudo chmod 666 /var/run/docker.sock
 

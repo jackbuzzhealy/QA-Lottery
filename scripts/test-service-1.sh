3fac45b14e8630a58ce87d144dd1a6302fbb3df0
Jack Healy <68054142+jackbuzzhealy@users.noreply.github.com>
2020-09-01 08:15:11 +0100

    Update test-service-1.sh

--- scripts/test-service-1.sh
+++ scripts/test-service-1.sh
@@ -1,3 +1,4 @@
 cd QA-Lottery/application1
+export DATABASE_URI=mysql+pymysql://root:megabrick55@34.105.176.207:3306/qa_lottery_db
 export TEST_DATABASE_URI=mysql+pymysql://root:megabrick55@34.105.176.207:3306/lottery_test_db
 pytest --cov applications
